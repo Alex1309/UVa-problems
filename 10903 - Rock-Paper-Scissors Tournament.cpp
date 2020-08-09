ac8357ffc2cc3c3c516e5425798c3bcb2da8fee5
Alex1309 <lopez.alexandra@correounivalle.edu.co>
2020-08-09 12:39:14 -0500

    Add files via upload
    Finished

--- 10903 - Rock-Paper-Scissors Tournament.cpp
+++ 10903 - Rock-Paper-Scissors Tournament.cpp
@@ -12,7 +12,7 @@ using namespace std;
 
 int main() {
 	int n,k;
-	 bool t = true;
+	bool t = true;
 
 	while(cin>>n)
 		if(n==0)
@@ -25,9 +25,11 @@ int main() {
 				t = false;
 			float players[n];
 			float same[n];
+			float different[n];
 			for(int i=0;i<n;i++){
 				players[i]=0;
 				same[i]=0;
+				different[i]=0;
 			}
 			int p1,p2;
 			string m1,m2;
@@ -37,33 +39,46 @@ int main() {
 				if(m1=="rock" and m2=="paper"){
 					players[p2-1]+=1;
 					same[p1-1]+=1;
+					different[p2-1]=1;
+					different[p1-1]=1;
 				}
 				else if(m1=="rock" and m2=="scissors"){
 					players[p1-1]+=1;
 					same[p2-1]+=1;
+					different[p2-1]=1;
+					different[p1-1]=1;
 				}
 				else if(m1=="paper" and m2=="scissors"){
 					players[p2-1]+=1;
 					same[p1-1]+=1;
+					different[p2-1]=1;
+					different[p1-1]=1;
 				}
 				else if(m1=="paper" and m2=="rock"){
 					players[p1-1]+=1;
 					same[p2-1]+=1;
+					different[p2-1]=1;
+					different[p1-1]=1;
 				}
 				else if(m1=="scissors" and m2=="paper"){
 					players[p1-1]+=1;
 					same[p2-1]+=1;
+					different[p2-1]=1;
+					different[p1-1]=1;
 				}
 				else if(m1=="scissors" and m2=="rock"){
 					players[p2-1]+=1;
 					same[p1-1]+=1;
+					different[p2-1]=1;
+					different[p1-1]=1;
 				}
 			}
 			for(int i=0;i<n;i++){
 				float d=(float)(players[i]+same[i]);
+				if(different[i]==0)
+					cout<<"-"<<endl;
+				else
 					cout << setprecision (3) << fixed << (players[i])/(d)<<endl;
-
 			}
-
 		}
 }
