7e3186a3b3187896283e7254b57f69682420e68b
Alex1309 <lopez.alexandra@correounivalle.edu.co>
2020-08-08 23:44:55 -0500

    Add files via upload
    Not finished, missing out <<"-" cases

--- 10903 - Rock-Paper-Scissors Tournament.cpp
+++ 10903 - Rock-Paper-Scissors Tournament.cpp
@@ -12,52 +12,60 @@ using namespace std;
 
 int main() {
 	int n,k;
-	while(cin>>n>>k)
+	while(cin>>n)
 		if(n==0)
 			break;
 		else{
+			cin>>k;
+			int t=0;
+			t++;
 
-	float players[n];
-	float same[n];
-	for(int i=0;i<n;i++){
-		players[i]=0;
-		same[i]=0;
-	}
-	int p1,p2;
-	string m1,m2;
-	int x =(k*n*(n-1))/2;
-	for(int i=0;i<x;i++){
-		cin>>p1>>m1>>p2>>m2;
-		if(m1=="rock" and m2=="paper"){
-			players[p2-1]+=1;
-			same[p1-1]+=1;
-		}
-		else if(m1=="rock" and m2=="scissors"){
-			players[p1-1]+=1;
-			same[p2-1]+=1;
-		}
-		else if(m1=="paper" and m2=="scissors"){
-			players[p2-1]+=1;
-			same[p1-1]+=1;
-		}
-		else if(m1=="paper" and m2=="rock"){
-			players[p1-1]+=1;
-			same[p2-1]+=1;
-		}
-		else if(m1=="scissors" and m2=="paper"){
-			players[p1-1]+=1;
-			same[p2-1]+=1;
-		}
-		else if(m1=="scissors" and m2=="rock"){
-			players[p2-1]+=1;
-			same[p1-1]+=1;
-		}
-	}
-	for(int i=0;i<n;i++){
-		float d=(float)(players[i]+same[i]);
-		cout << setprecision (3) << fixed << (players[i])/(d)<<endl;
-	}
+			float players[n];
+			float same[n];
+			for(int i=0;i<n;i++){
+				players[i]=0;
+				same[i]=0;
+			}
+			int p1,p2;
+			string m1,m2;
+			int x =(k*n*(n-1))/2;
+			for(int i=0;i<x;i++){
+				cin>>p1>>m1>>p2>>m2;
+				if(m1=="rock" and m2=="paper"){
+					players[p2-1]+=1;
+					same[p1-1]+=1;
+				}
+				else if(m1=="rock" and m2=="scissors"){
+					players[p1-1]+=1;
+					same[p2-1]+=1;
+				}
+				else if(m1=="paper" and m2=="scissors"){
+					players[p2-1]+=1;
+					same[p1-1]+=1;
+				}
+				else if(m1=="paper" and m2=="rock"){
+					players[p1-1]+=1;
+					same[p2-1]+=1;
+				}
+				else if(m1=="scissors" and m2=="paper"){
+					players[p1-1]+=1;
+					same[p2-1]+=1;
+				}
+				else if(m1=="scissors" and m2=="rock"){
+					players[p2-1]+=1;
+					same[p1-1]+=1;
+				}
+			}
+			for(int i=0;i<n;i++){
+				float d=(float)(players[i]+same[i]);
+				if((players[i])/(d)==0){
+					cout<<"-"<<endl;
+				}else{
+					cout << setprecision (3) << fixed << (players[i])/(d)<<endl;
+				}
+			}
+			if(t>=1)
+				cout<<endl;
 
-	}
-
+		}
 }
