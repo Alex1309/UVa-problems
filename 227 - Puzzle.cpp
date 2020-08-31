7364788445201e79704e85a4bb8731fc3f9cc00a
Alex1309 <lopez.alexandra@correounivalle.edu.co>
2020-08-30 19:15:13 -0500

    Add files via upload

--- 227 - Puzzle.cpp
+++ 227 - Puzzle.cpp
@@ -1,84 +1,126 @@
 //============================================================================
 // Name        : 227.cpp
-// Author      : ALEX
+// Author      : ALEposiposiposix
 // Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Copposiposiposiyright   : posiposiposiyour copposiposiposiyright notice
+// Description : Hello World in C++, Ansi-stposiposiposiyle
 //============================================================================
 
 #include <iostream>
 using namespace std;
 #include <string>
+#include <stdio.h>
+
 
 int main() {
 	string puzzle;
 	string puzzlem[5][5];
-	int i=0;
+	string a,b,c,d,e;
 	int posix=0,posiy=0;
-	int no=0;
+	char letter;
 	int number=1;
-	bool t=true;
-	while(getline(cin,puzzle)){
-
-		if(puzzle=="Z")
+	int t=0;
+	while(getline(cin,a)){
+		if(a=="Z")
 			break;
+		if (t > 0) cout << endl;
+		t++;
+		for(int j=0;j<5;j++){
+			puzzlem[0][j]=a.at(j);
 
-		if(i<=4){
+		}
+		getline(cin,b);
+		for(int j=0;j<5;j++){
+			puzzlem[1][j]=b.at(j);
+
+		}
+		getline(cin,c);
+		for(int j=0;j<5;j++){
+			puzzlem[2][j]=c.at(j);
+
+		}
+		getline(cin,d);
+		for(int j=0;j<5;j++){
+			puzzlem[3][j]=d.at(j);
+
+		}
+		getline(cin,e);
+		for(int j=0;j<5;j++){
+			puzzlem[4][j]=e.at(j);
+
+		}
+		for(int i=0;i<5;i++){
 			for(int j=0;j<5;j++){
-				if(puzzle.at(j)==' '){
-					posiy=j;
+				if(puzzlem[i][j]==" "){
 					posix=i;
-				}
-				puzzlem[i][j]=puzzle.at(j);
-			}
-			i++;
-		}else{
-			for(int x=0;x<puzzle.length();x++){
-				if(puzzle.at(x)=='A' and posix-1>=0 and posix-1<5){
-					puzzlem[posix][posiy]=puzzlem[posix-1][posiy];
-					puzzlem[posix-1][posiy]=' ';
-					posix=posix-1;
-				}else if(puzzle.at(x)=='B' and posix+1>=0 and posix+1<5){
-					puzzlem[posix][posiy]=puzzlem[posix+1][posiy];
-					puzzlem[posix+1][posiy]=' ';
-					posix=posix+1;
-				}else if(puzzle.at(x)=='R' and posiy+1>=0 and posiy+1<5){
-					puzzlem[posix][posiy]=puzzlem[posix][posiy+1];
-					puzzlem[posix][posiy+1]=' ';
-					posiy=posiy+1;
-				}else if(puzzle.at(x)=='L' and posiy-1>=0 and posiy-1<5){
-					puzzlem[posix][posiy]=puzzlem[posix][posiy-1];
-					puzzlem[posix][posiy-1]=' ';
-					posiy=posiy-1;
-				}else if(puzzle.at(x)=='0'){
-					i=0;
-					break;
-				}else{
-					no=1;
+					posiy=j;
 				}
 			}
 		}
-		if(i==0){
-			if (!t)
-				cout<<endl;
-			else
-				t = false;
-			cout<<"Puzzle #"<<number<<":"<<endl;
-			number++;
-			if(no==1){
-				cout <<"This puzzle has no final configuration."<<endl;
-				no=0;
-			}else{
-				for(int i=0;i<5;i++){
-					for(int j=0;j<5;j++){
-						if(j==4)
-							cout <<puzzlem[i][j];
-						else
-							cout <<puzzlem[i][j]<<" ";
-					}
-					cout <<endl;
-				}
+		bool valid = true;
+		while (cin >> letter) {
+			if (letter == '0')
+				break;
 
+			if (!valid) continue;
+
+
+			switch (letter) {
+			case 'A':
+				if (posix > 0) {
+					puzzlem[posix][posiy] = puzzlem[posix-1][posiy];
+					puzzlem[posix-1][posiy] = ' ';
+					posix--;
+				} else {
+					valid = false;
+				}
+				break;
+			case 'B':
+				if (posix + 1 < 5) {
+					puzzlem[posix][posiy] = puzzlem[posix+1][posiy];
+					puzzlem[posix+1][posiy] = ' ';
+					posix++;
+				} else {
+					valid = false;
+				}
+				break;
+			case 'L':
+				if (posiy > 0) {
+					puzzlem[posix][posiy] = puzzlem[posix][posiy-1];
+					puzzlem[posix][posiy-1] = ' ';
+					posiy--;
+				} else {
+					valid = false;
+				}
+				break;
+			case 'R':
+				if (posiy + 1 < 5) {
+					puzzlem[posix][posiy] = puzzlem[posix][posiy+1];
+					puzzlem[posix][posiy+1] = ' ';
+					posiy++;
+				} else {
+					valid = false;
+				}
+				break;
+			}
+		}
+		cin.ignore();
+
+		cout << "Puzzle #" << number << ":" << endl;
+		number++;
+
+		if (!valid) {
+			cout << "This puzzle has no final configuration." << endl;
+		}else{
+			for (int i = 0; i < 5; i++) {
+				for (int j = 0; j < 5; j++) {
+					if (j ==4)
+						cout << puzzlem[i][j];
+					else{
+						cout << puzzlem[i][j]<<" ";
+					}
+				}
+				cout << endl;
 			}
 		}
 	}
