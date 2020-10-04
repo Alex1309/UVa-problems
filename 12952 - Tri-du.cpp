d6a3f2f6390d06995bcb7c0c8ad1e2460d42c518
Alex1309 <lopez.alexandra@correounivalle.edu.co>
2020-10-04 18:51:19 -0500

    Add files via upload

---
+++ 12952 - Tri-du.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : 12952.cpp
+// Author      : ALEX
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+ int a,b;
+ while(cin>>a>>b){
+	 if(a>b){
+		 cout<<a<<endl;
+	 }else if(a<b){
+		 cout<<b<<endl;
+
+	 }else if(a==b){
+		 cout <<a<<endl;
+	 }
+ }
+}
