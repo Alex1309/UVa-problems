92bbc3aec5cf7d612196a0501d322e898a14bf6e
Alex1309 <lopez.alexandra@correounivalle.edu.co>
2021-02-28 16:10:47 -0500

    Add files via upload
    Almost finished

--- 141 - The Spot Game.cpp
+++ 141 - The Spot Game.cpp
@@ -8,21 +8,95 @@
 
 #include <iostream>
 using namespace std;
+#include <map>
+
+#include <stdio.h>
 
 int main() {
-	int a;
-	cin >>a;
-	int m[a][a];
-	for(int i=0;i<a*2;i++){
+	int n;
+	cin >>n;
+	int table[n][n];
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			table[i][j]=0;
+		}
+	}
+
+	map<string,int> ifrepeated;
+
+
+	int endWhile=2*n;
+	int move =1;
+	int moveWinner=0;
+	int end=0;
+	while(endWhile!=0){
+
 		int x,y;
 		string s;
 		cin>>x>>y>>s;
 		if(s == "+"){
-			m[x-1][y-1]= 1;
+			table[x-1][y-1]= 1;
 		}else{
-			m[x-1][y-1]= 0;
+			table[x-1][y-1]= 0;
+		}
+
+		string v;
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				v += (table[i][j]+'0');
+			}
+		}
+		if (ifrepeated[v]==1 and end==0){
+			end=1;
+			moveWinner=move;
 
 		}
 
+		int auxTable[n][n];
+		int auxTemp[n][n];
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				auxTemp[i][j]=table[i][j];
+			}
+		}
+		for(int z=0;z<4;z++){
+			int iAux=0;
+			int jAux=n-1;
+			for(int i=0;i<n;i++){
+				for(int j=0;j<n;j++){
+					auxTable[iAux][jAux]=auxTemp[i][j];
+					iAux++;
+				}
+				jAux--;
+				iAux=0;
+			}
+			for(int i = 0; i < n; i++){
+				for(int j = 0; j < n; j++){
+					auxTemp[i][j]=auxTable[i][j];
+				}
+			}
+
+			string auxV="";
+			for(int i = 0; i < n; i++){
+				for(int j = 0; j < n; j++){
+					auxV += (auxTable[i][j]+'0');
+				}
+			}
+
+			ifrepeated[auxV]=1;
+
+		}
+
+		endWhile--;
+		move++;
 	}
+
+	if(end==1){
+		cout<<moveWinner;
+		cout<<"lost";
+
+	}else{
+		cout <<"draw";
+	}
+
 }
